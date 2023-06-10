ecc1c5313c422c40393f62ea150d2248725905e9
Andrei Melnikov <125220410+amelnikov777@users.noreply.github.com>
2023-06-10 23:27:50 +0300

    v 0.2

--- bilingua.go
+++ bilingua.go
@@ -9,23 +9,27 @@ import (
 	gt "github.com/bas24/googletranslatefree"
 )
 
+var ReadSlice []string
+var Filesave string
+
 func main() {
-	fmt.Println("Сохранить файл из word в кодировке Юникод UTF-8 без разрывов строк.")
-	fmt.Println("Имя файла без пробелов.")
+	fmt.Println("Перед переводом сохранить txt файл из word в кодировке Юникод UTF-8 без разрывов строк.")
+	ReadSlice = make([]string, 0)
 
-	//	type readst struct {
-	// StringOriginal, StringAfter string
-	// }
-	ReadSlice := make([]string, 0)
-	var filename string
+	ReadFile()
+	Translate()
+	SaveFile()
+}
 
+func ReadFile() { //This function reads user txt file and writes it to ReadSlice, also function generates file name to save.
 	fmt.Print("Please input the name of file to read: ")
-	fmt.Scan(&filename)
-	fmt.Println(" ", filename, " -> ", "bi"+filename)
+	filename, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	filename = strings.Trim(filename, "\r\n")
 	f, err := os.Open(filename)
+	BiFilename := strings.Split(filename, ".")
 	if err == nil {
+		fmt.Println(" " + filename + " -> " + BiFilename[0] + "_bi." + BiFilename[1])
 		readdata := bufio.NewScanner(f)
-
 		for readdata.Scan() {
 			Read := strings.Split(readdata.Text(), ".")
 			for _, readapeend := range Read {
@@ -37,24 +41,15 @@ func main() {
 			}
 		}
 	} else {
-		fmt.Println("File open error! Please check the filename.")
+		fmt.Println("File open error! Please check the filename. " + filename)
 	}
 	fmt.Println("Прочитано строк: ", len(ReadSlice))
+	Filesave = (BiFilename[0] + "_bi." + BiFilename[1])
+}
 
-	for i, ii := range ReadSlice {
-		// you can use "auto" for source language
-		// so, translator will detect language
-		if len(ii) > 3 {
-			result, _ := gt.Translate(ii, "en", "ru")
-			ReadSlice[i] = "[" + result + "] " + ReadSlice[i]
-			// } else {
-			// ReadSlice[i] = "\n"
-		}
-		fmt.Printf("\rПереведено: %d", i)
-	}
-	filesave := "bi" + filename
-	fs, errs := os.Create(filesave)
-	if errs != nil {
+func SaveFile() { //It saves file from final []ReadSlice with FileSave name
+	fs, err := os.Create(Filesave)
+	if err != nil {
 		panic(err)
 	}
 	defer fs.Close()
@@ -64,5 +59,17 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+	}
+}
+
+func Translate() {
+	for i, ii := range ReadSlice {
+		// you can use "auto" for source language
+		// so, translator will detect language
+		if len(ii) > 3 {
+			result, _ := gt.Translate(ii, "en", "ru")
+			ReadSlice[i] = " [" + result + "] " + ReadSlice[i]
+		}
+		fmt.Printf("\rПереведено: %d", i+1)
 	}
 }
